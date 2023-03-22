5b9157e7d783a7e44366540a5c520d36b360d1ca
BiancoSofia24 <b.sofia2124@gmail.com>
2023-03-22 00:12:54 -0300

    adding footer and dev branch

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ npm i
 ```
 npm install
 ```
+To run the project use
+```
+npm start
+```
+
+Visit the page at [Express-Verce](https://express-vercel-sb.vercel.app/)
--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = 5000;
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
-  res.send('<h1>Project express-vercel<h1>');
   res.sendFile(__dirname + '/index.html');
 });
 
