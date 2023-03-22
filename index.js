d5e505c8114d23426e5921b1b46d8e0ada218948
BiancoSofia24 <b.sofia2124@gmail.com>
2023-03-21 23:54:31 -0300

    udpate package.json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 5000;
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
-  res.send('Project express-vercel');
+  res.send('<h1>Project express-vercel<h1>');
   res.sendFile(__dirname + '/index.html');
 });
 
