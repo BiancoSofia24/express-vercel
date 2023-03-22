a172673feda7690af5fb9de3d0d621d79a7e7843
BiancoSofia24 <b.sofia2124@gmail.com>
2023-03-21 23:29:51 -0300

    installing express & nodemon

---
+++ README.md
@@ -0,0 +1,9 @@
+## Readme
+
+To install all the packages run
+```
+npm i 
+```
+```
+npm install
+```
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Express-Vercel Project')
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res) => {
+  res.send('Project express-vercel');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
